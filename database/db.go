800786059d28e064e1edc62bdc09ede1f6f69cd7
ch-m-n <chaungo7703@gmail.com>
2023-08-02 17:31:33 +0700

    EC2 testing

--- database/db.go
+++ database/db.go
@@ -5,28 +5,13 @@ import (
 	"gorm.io/gorm"
 )
 
-// func ConnDB() *gorm.DB {
-
-// 	dsn := "host=" + "lingo.co05gj6uni3r.us-east-1.rds.amazonaws.com" +
-// 		" user=" + "postgres" +
-// 		" password=" + "Hades330!" +
-// 		" dbname=" + "postgres" +
-// 		" port=" + "5432" + " sslmode=require TimeZone=Asia/Shanghai"
-// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-// 	if err != nil {
-// 		panic("Failed to connect to database")
-// 	}
-
-// 	return db
-// }
-
 func ConnDB() *gorm.DB {
 
-	dsn := "host=" + "localhost" +
-		" user=" + "admin" +
-		" password=" + "6457" +
-		" dbname=" + "lingo" +
-		" port=" + "5432" + " sslmode=disable TimeZone=Asia/Shanghai"
+	dsn := "host=" + "lingo.co05gj6uni3r.us-east-1.rds.amazonaws.com" +
+		" user=" + "postgres" +
+		" password=" + "Hades330!" +
+		" dbname=" + "postgres" +
+		" port=" + "5432" + " sslmode=require TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database")
@@ -34,3 +19,18 @@ func ConnDB() *gorm.DB {
 
 	return db
 }
+
+// func ConnDB() *gorm.DB {
+
+// 	dsn := "host=" + "localhost" +
+// 		" user=" + "admin" +
+// 		" password=" + "6457" +
+// 		" dbname=" + "lingo" +
+// 		" port=" + "5432" + " sslmode=disable TimeZone=Asia/Shanghai"
+// 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		panic("Failed to connect to database")
+// 	}
+
+// 	return db
+// }

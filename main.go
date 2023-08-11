d676e861a74eb268cd038cf7682e0a8c51bd2e11
ch-m-n <chaungo7703@gmail.com>
2023-08-11 17:32:49 +0700

    fix cors

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func initRouter() *gin.Engine {
 	router.Use(cors.Default())
 	path := router.Group("/")
 	{
+		path.Use(cors.Default())
 		path.GET("/",controllers.Home)
 	}
 	api := router.Group("/api")
@@ -36,6 +37,7 @@ func initRouter() *gin.Engine {
 		api.POST("/user/login", controllers.VerifyUser)
 		secured := api.Group("/secured").Use(middlewares.Auth())
 		{
+			secured.Use(cors.Default())
 			secured.GET("/user", controllers.GetUser)
 			secured.GET("/user/word/get", controllers.GetWord)
 			secured.GET("/user/content/get", controllers.GetContents)

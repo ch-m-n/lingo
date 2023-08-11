009812f7b14cbe7456ca9cc63c4cdebe86fb8903
ch-m-n <chaungo7703@gmail.com>
2023-08-11 17:17:54 +0700

    fix cors

--- controllers/note-controllers.go
+++ controllers/note-controllers.go
@@ -9,6 +9,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// func GetAllNotes(c *gin.Context) {
+// 	words := new(models.InputGetNote)
+// 	e := c.BindJSON(&words)
+// 	if e != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"message": e.Error()})
+// 		return
+// 	}
+// 	var note models.Note
+// 	var notes models.OutputGetNote
+// 	for i := 0; i < len(words.Words); i++ {
+// 		future := async.Exec(func() interface{} {
+// 			return database.ConnDB().Table("note").Raw("SELECT * FROM note "+
+// 														"WHERE user_id='"+words.User_id+"'").Scan(&note)
+// 		})
+// 		future.Await()
+// 		notes.Notes = append(notes.Notes, note)
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"data": notes, "status": http.StatusOK})
+// }
+
 func GetNote(c *gin.Context) {
 	words := new(models.InputGetNote)
 	e := c.BindJSON(&words)

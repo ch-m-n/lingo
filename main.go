dd35296c3c58152ffcb7160cacac8b57d9358e97
ch-m-n <chaungo7703@gmail.com>
2023-08-11 17:19:21 +0700

    fix cors

--- main.go
+++ main.go
@@ -26,26 +26,22 @@ func main() {
 func initRouter() *gin.Engine {
 	router := gin.Default()
 	router.Use(cors.Default())
-	api := router.Group("/")
+	api := router.Group("/api")
 	{
-		api.GET("/",controllers.Home)
-		apiv1 := api.Group("/api")
+		api.POST("/user/register", controllers.CreateUser)
+		api.POST("/user/login", controllers.VerifyUser)
+		secured := api.Group("/secured").Use(middlewares.Auth())
 		{
-			apiv1.POST("/user/register", controllers.CreateUser)
-			apiv1.POST("/user/login", controllers.VerifyUser)
-			secured := apiv1.Group("/secured").Use(middlewares.Auth())
-			{
-				secured.GET("/user", controllers.GetUser)
-				secured.GET("/user/word/get", controllers.GetWord)
-				secured.GET("/user/content/get", controllers.GetContents)
-				secured.GET("/user/literacy/get", controllers.GetWordLevel)
-				secured.GET("/user/note/get", controllers.GetNote)
-				// secured.POST("/user/word/add", controllers.AddWord)
-				secured.POST("/user/content/add", controllers.AddContents)
-				secured.POST("/user/content/edit", controllers.EditContent)
-				secured.POST("/user/literacy/add", controllers.AddWordLevel)
-				secured.POST("/user/note/edit", controllers.EditNote)
-			}
+			secured.GET("/user", controllers.GetUser)
+			secured.GET("/user/word/get", controllers.GetWord)
+			secured.GET("/user/content/get", controllers.GetContents)
+			secured.GET("/user/literacy/get", controllers.GetWordLevel)
+			secured.GET("/user/note/get", controllers.GetNote)
+			// secured.POST("/user/word/add", controllers.AddWord)
+			secured.POST("/user/content/add", controllers.AddContents)
+			secured.POST("/user/content/edit", controllers.EditContent)
+			secured.POST("/user/literacy/add", controllers.AddWordLevel)
+			secured.POST("/user/note/edit", controllers.EditNote)
 		}
 	}
 

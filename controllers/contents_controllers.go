8f672b481fb578d672ace90544ec8a2ac33f1248
ch-m-n <chaungo7703@gmail.com>
2023-09-25 20:43:21 +0700

    string processing

--- controllers/contents_controllers.go
+++ controllers/contents_controllers.go
@@ -16,12 +16,12 @@ import (
 func stringProcessor(s string) []string {
 	var words_list []string
 	// var nonAlphanumericRegex = regexp.MustCompile(`[^a-zA-z0-9 ]+`)
-	var nonAlphanumericRegex = regexp.MustCompile(`([^{\p{L}}{0-9}])`)
+	var nonAlphanumericRegex = regexp.MustCompile(`([^{\p{L}}{0-9}]+)`)
 	var punctuationRegex = regexp.MustCompile(`\p{P}`)
 	words := nonAlphanumericRegex.ReplaceAllString(s, "")
 	words = strings.TrimSpace(words)
 	words = punctuationRegex.ReplaceAllString(words, "")
-	list := strings.Split(words, "")
+	list := strings.Split(words, " ")
 	words_list = append(words_list, list...)
 	return words_list
 }

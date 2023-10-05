ffc9be336167323c3264b034f2b5c9db4def9cfe
ch-m-n <chaungo7703@gmail.com>
2023-10-05 10:50:16 +0700

    CI/CD integration for aws

--- database/db.go
+++ database/db.go
@@ -7,6 +7,7 @@ import (
 )
 
 func ConnDB() *sqlx.DB {
+	
 	db, err := sqlx.Connect("postgres",
 		"user=postgres password=Hades330! dbname=postgres host=lingo.co05gj6uni3r.us-east-1.rds.amazonaws.com port=5432 sslmode=require")
 	if err != nil {
